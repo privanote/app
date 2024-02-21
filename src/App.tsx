363207a1bef0cdc13771db85be11d749271ae067
Olivier Goulet <olivier_goulet@icloud.com>
2024-02-20 20:14:00 -0500

    refactor: remove previewer from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { Previewer } from "./modules/Previewer";
-
-const markdown = `
-# Heading
-
-This is **bold** text. 
-
-This is *italic* text.
-
-This is <ins>underlined</ins> text.
-`;
-
 function App() {
   return (
     <>
-      <Previewer input={markdown} />
     </>
   )
 }
