7f011354066f6d064bee8bcfa9209c905d7e63d4
Olivier Goulet <olivier_goulet@icloud.com>
2024-02-21 20:19:52 -0500

    fix: type errors

--- src/modules/Previewer.tsx
+++ src/modules/Previewer.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { previewService } from "../services/preview";
+import parseReactElementFromHtml from "html-react-parser"
 
 type PreviewerProps = {
   input: string;
@@ -9,9 +10,10 @@ type PreviewerProps = {
   * The previewer which displays the formatted markup text.
   */
 export function Previewer({ input }: PreviewerProps) {
-  // Parse the input received.
+  // Parse the input received into an html string using the preview service.
+  // Then, convert the html string into JSX.
   const content = useMemo(() => {
-    return previewService.parse(input, "markdown")
+    return parseReactElementFromHtml(previewService.parse(input, "markdown"))
   }, [input]); 
 
   return (
