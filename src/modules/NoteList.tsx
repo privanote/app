61fb10f90802c44516edb7be99d809355400176a
Olivier Goulet <olivier_goulet@icloud.com>
2024-02-21 21:27:07 -0500

    refactor: flatten note component with less composition

--- src/modules/NoteList.tsx
+++ src/modules/NoteList.tsx
@@ -1,27 +1,43 @@
-import { Note } from './noteList/Note';
+import { Note } from "./noteList/Note";
+import { FilePlus as CreateNoteIcon } from "react-feather";
+
+type SpacerProps = {
+  /** Only accepts Tailwind height classes.*/
+  className: `h-${number}`;
+};
+
+function Spacer(props: SpacerProps) {
+  return <div {...props}></div>
+}
 
 export function NoteList() {
   return (
-    <div className="relative h-screen border-r border-zinc-800 pt-8 pl-8">
-      <form className="pb-6 pr-8">
-        <input className="text-xs px-2 py-1 w-full rounded-md border border-zinc-800 bg-zinc-900" placeholder="Search Notes" />
-      </form>
-      <ul>
-        <li className="font-bold text-3xl px-8 py-4 -ml-8 border-y border-zinc-900 hover:bg-zinc-900 cursor-pointer">
-          <div className="text-center">+</div>
-        </li>
+    <div className="relative h-screen border-r border-zinc-800">
+      <div className="pt-8 px-8">
+        <form>
+          <input className="text-xs px-2 py-1 w-full rounded-md border border-zinc-800 bg-zinc-900" placeholder="Search Notes" />
+        </form>
+        <Spacer className="h-3" />
+        <ul className="flex justify-end">
+          <li>
+            <button className="flex gap-1 items-center px-2 py-1 border border-zinc-800 rounded-md text-xs text-zinc-300 hover:border-zinc-400 transition-colors">
+              <span>Create</span>
+              <CreateNoteIcon size={18} />
+            </button>
+          </li>
+        </ul>
+        <Spacer className="h-3" />
+      </div>
+      <ul className="pl-8">
+        <Note heading="Heading" thumbnailUrl="https://fastly.picsum.photos/id/884/200/200.jpg?hmac=BEameeYahafzsG-UCcyxl-la3nd2GolgYlQgWpd5krk">
+          Lorem ipsum dolor something something...
+        </Note>
         { Array.from({ length: 8 }).map(() => (
-          <Note>
-            <Note.Heading>
-              Heading
-            </Note.Heading>
-            <Note.Summary>
-              Lorem ipsum dolor something something...
-            </Note.Summary>
+          <Note heading="Heading">
+            Lorem ipsum dolor something something...
           </Note>
         ))}
       </ul>
     </div>
   );
 }
-
--- src/modules/noteList/Note.tsx
+++ src/modules/noteList/Note.tsx
@@ -1,25 +1,20 @@
-export function Note({ children }: React.PropsWithChildren) {
+type NoteProps = React.PropsWithChildren<{
+  heading?: string;
+  thumbnailUrl?: string;
+}>
+
+export function Note({ heading = "Untitled", thumbnailUrl, children }: React.PropsWithChildren) {
   return (
-    <li className="px-8 py-4 -ml-8 border-y border-zinc-900 hover:bg-zinc-900 cursor-pointer">
-      {children}
+    <li className="flex items-center gap-2 overflow-hidden max-h-32 px-8 py-4 -ml-8 border border-zinc-900 hover:bg-zinc-900 hover:border-zinc-400 transition-colors cursor-pointer">
+      <div className="w-full">
+        <h3 className="font-medium">{heading}</h3>
+        <p className="text-xs text-zinc-300">
+          {children}
+        </p>
+      </div>
+      <div className="w-12 h-12 shrink-0">
+        { thumbnailUrl ? <img className="aspect-square w-full h-full object-cover" src={thumbnailUrl} /> : null }
+      </div>
     </li>
   );
 }
-
-function Heading({ children }: React.PropsWithChildren) {
-  return (
-    <h3 className="font-medium">{children}</h3>
-  );
-}
-
-function Summary({ children }: React.PropsWithChildren) {
-  return (
-    <p className="text-xs text-zinc-300">
-      {children}
-    </p>
-  );
-}
-
-Note.Heading = Heading;
-Note.Summary = Summary;
-
